6e7b0597cd45673090ab9e3c46a8cb25bf395bfd
leecoders <als-1994@hanmail.net>
2019-09-17 18:24:58 +0900

    docs: 체크리스트 링크 변경

---
+++ public/javascripts/components/MiniCarousel/MiniCarousel.js
@@ -0,0 +1,1 @@
+import miniCarouselTemplate from "./template.js";
---
+++ public/javascripts/components/MiniCarousel/template.js
@@ -0,0 +1,30 @@
+export default /*html*/ `
+<section>
+<div class="main-container">
+  <div class="mini-carousel-container">
+    <div class="mini-carousel-left-button-area">
+      <button
+        id="mini-carousel-left-button"
+        class="mini-carousel-button"
+      ></button>
+    </div>
+    <div class="mini-carousel-center">
+      <div class="img-container">
+        <ul class="img-deque">
+          <li id="a"><img class="mini-carousel-image"></img></li>
+          <li id="b"><img class="mini-carousel-image"></img></li>
+          <li id="c"><img class="mini-carousel-image"></img></li>
+          <li id="d"><img class="mini-carousel-image"></img></li>
+        </ul>
+      </div>
+    </div>
+    <div class="mini-carousel-right-button-area">
+      <button
+        id="mini-carousel-right-button"
+        class="mini-carousel-button"
+      ></button>
+    </div>
+  </div>
+</div>
+</section>
+`;
