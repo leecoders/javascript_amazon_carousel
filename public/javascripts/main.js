48bd2fff912c4e1a54dea334119c29e0b5b963a5
leecoders <als-1994@hanmail.net>
2019-09-17 18:15:26 +0900

    update: 미니 캐로셀 클래스로 리팩토링

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,41 +1,55 @@
 import { $ } from "./util/util.js";
 import { miniImages } from "./miniImage.js";
 
-let size = 4;
-let left = -20;
-let trans = 0;
-const renderImages = () => {
-  for (let i = 0; i < 4; ++i) {
-    $(".img-deque").children[i].firstChild.src = miniImages[i].src;
+class MiniCarousel {
+  constructor(miniImages, listCnt, listWidth) {
+    this.miniImages = miniImages;
+    this.listCnt = listCnt;
+    this.leftEnd = -listWidth;
+    this.transDist = 0;
   }
-};
 
-renderImages();
+  renderImages = () => {
+    for (let i = 0; i < this.listCnt; ++i) {
+      $(".img-deque").children[i].firstChild.src = this.miniImages[i].src;
+    }
+  };
 
-const moveImagesToLeft = () => {
-  left -= 20;
-  trans += 20;
-  const cloneNode = $(".img-deque").children[size - 1].cloneNode(true);
-  $(".img-deque").insertAdjacentElement("afterbegin", cloneNode);
-  $(".img-deque").style.left = left + "rem";
-  $(".img-deque").style.transform = `translate(${trans}rem)`;
-  $(".img-deque").removeChild($(".img-deque").children[size]);
-};
+  moveImagesToLeft = () => {
+    this.leftEnd -= 20;
+    this.transDist += 20;
+    const cloneNode = $(".img-deque").children[this.listCnt - 1].cloneNode(
+      true
+    );
+    $(".img-deque").insertAdjacentElement("afterbegin", cloneNode);
+    $(".img-deque").style.left = this.leftEnd + "rem";
+    $(".img-deque").style.transform = `translate(${this.transDist}rem)`;
+    $(".img-deque").removeChild($(".img-deque").children[this.listCnt]);
+  };
 
-const moveImagesToRight = () => {
-  left += 20;
-  trans -= 20;
-  const cloneNode = $(".img-deque").children[0].cloneNode(true);
-  $(".img-deque").insertAdjacentElement("beforeend", cloneNode);
-  $(".img-deque").style.left = left + "rem";
-  $(".img-deque").style.transform = `translate(${trans}rem)`;
-  $(".img-deque").removeChild($(".img-deque").children[0]);
-};
+  moveImagesToRight = () => {
+    this.leftEnd += 20;
+    this.transDist -= 20;
+    const cloneNode = $(".img-deque").children[0].cloneNode(true);
+    $(".img-deque").insertAdjacentElement("beforeend", cloneNode);
+    $(".img-deque").style.left = this.leftEnd + "rem";
+    $(".img-deque").style.transform = `translate(${this.transDist}rem)`;
+    $(".img-deque").removeChild($(".img-deque").children[0]);
+  };
 
-$("#mini-carousel-left-button").addEventListener("click", () => {
-  moveImagesToLeft();
-});
+  render = () => {
+    this.renderImages();
+    $("#mini-carousel-left-button").addEventListener(
+      "click",
+      this.moveImagesToLeft
+    );
 
-$("#mini-carousel-right-button").addEventListener("click", () => {
-  moveImagesToRight();
-});
+    $("#mini-carousel-right-button").addEventListener(
+      "click",
+      this.moveImagesToRight
+    );
+  };
+}
+
+const mini = new MiniCarousel(miniImages, 4, 20);
+mini.render();
