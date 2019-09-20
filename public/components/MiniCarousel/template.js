3012bed365f6fa1c197985572d4f79899dbdbda1
leecoders <als-1994@hanmail.net>
2019-09-20 10:21:43 +0900

    feat: 미니 캐로셀 텍스트

--- public/components/MiniCarousel/template.js
+++ public/components/MiniCarousel/template.js
@@ -23,9 +23,16 @@ export default /*html*/ `
       ></button>
     </div>
     <div class="mini-text-container">
-      <div class="mini-title"></div>
-      <div class="mini-content"></div>
-      <div class="mini-tail"></div>
+      <div class="mini-text-title">
+        Amazon Originals, exclusively on Prime Video
+      </div>
+      <div class="mini-text-content">
+        Prime Video is the only place where you can watch Amazon Original series like "The Marvelous Mrs. Maisel", "Tom Clancy's Jack Ryan", "Homecoming", and "The Man in the High Castle".
+      </div>
+      <div class="mini-text-tail">
+        <a href="https://www.amazon.com/gp/video/storefront/ref=dvm_us_aq_np_dhb_be_optorigt1?ie=UTF8&merchId=originals1">Explore Prime Video</a>
+        <button type="button" class="mini-carousel-triangle"></button>
+      </div>
     </div>
   </div>
 `;
