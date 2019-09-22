9466fb99824abfb1e68f7fae446de43b36a8450a
leecoders <als-1994@hanmail.net>
2019-09-22 20:13:58 +0900

    add: Nav 컴포넌트
    상단 배너 역할

--- web/src/index.js
+++ web/src/index.js
@@ -1,5 +1,6 @@
 // import "@babel/polyfill"; // for web-pack
 import { Header } from "./components/Header/Header.js";
+import { Nav } from "./components/Nav/Nav.js";
 import { Section } from "./components/Section/Section.js";
 import { Footer } from "./components/Footer/Footer.js";
 import { MiniCarousel } from "./components/MiniCarousel/MiniCarousel.js";
@@ -9,6 +10,7 @@ import { miniImages } from "./image_objs/miniImage.js";
 import mainImagesObj from "./image_objs/mainImage.js";
 
 const header = new Header($("header"));
+const nav = new Nav($("nav"));
 const section = new Section($("section"));
 const footer = new Footer($("footer"));
 
