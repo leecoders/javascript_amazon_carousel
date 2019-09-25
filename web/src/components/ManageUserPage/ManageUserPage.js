a8d57f377937fff40d85611a6bee5b4f9be9268b
leecoders <als-1994@hanmail.net>
2019-09-25 16:34:47 +0900

    feat: 관리자 페이지 -> 사용자 관리 -> 유저 정보
    서버에서 받아오도록 프론트에서 fetch, 백엔드에서 라우팅 -> 모델 객체 메서드에서 DB select해서 응답

--- web/src/components/ManageUserPage/ManageUserPage.js
+++ web/src/components/ManageUserPage/ManageUserPage.js
@@ -1,6 +1,7 @@
 import manageUserPageTemplate from "./template.js";
 import { ListUser } from "../../components/ListUser/ListUser.js";
 import { $ } from "../../utils/util.js";
+import { fetchAllUsers } from "../../utils/fetch.js";
 
 class ManageUserPage {
   constructor(parentElement) {
@@ -10,20 +11,53 @@ class ManageUserPage {
   }
 
   setUserList = () => {
-    const userLength = 5; // 임시로 값 넣어줌
-    for (let i = 0; i < userLength; ++i) {
+    for (let i = 0; i < this.listLength; ++i) {
       this.userListContainer.insertAdjacentHTML(
         "beforeend",
         /*html*/ `<div id="user-list-${i}" class="user-list"></div>`
       );
-      this.listUserArray.push(new ListUser($(`#user-list-${i}`), i));
+      this.listUserArray.push(
+        new ListUser(
+          $(`#user-list-${i}`),
+          i,
+          this.usersInfo[i].USER_ID,
+          this.usersInfo[i].USER_NAME,
+          this.usersInfo[i].USER_GRADE
+        )
+      );
     }
   };
 
-  render() {
+  setUsersInfo = async () => {
+    const results = await fetchAllUsers();
+    if (results.message == "success") {
+      this.usersInfo = results.data;
+      this.listLength = results.data.length;
+    } else {
+      // 에러 처리
+    }
+  };
+
+  setEvent = () => {
+    this.userListContainer.addEventListener("click", e => {
+      if (e.target.className == "user-grade-up-button") {
+        const targetIdx = e.target.id.split("up-")[1];
+        const userId = this.usersInfo[targetIdx].USER_ID;
+        console.log(userId);
+      }
+      if (e.target.className == "user-grade-down-button") {
+        const targetIdx = e.target.id.split("down-")[1];
+        const userId = this.usersInfo[targetIdx].USER_ID;
+      }
+    });
+  };
+
+  async render() {
     this.parentElement.insertAdjacentHTML("beforeend", manageUserPageTemplate);
     this.userListContainer = $(".user-list-container");
+    await this.setUsersInfo();
     this.setUserList();
+    this.setEvent();
   }
 }
 
--- web/src/utils/fetch.js
+++ web/src/utils/fetch.js
@@ -13,4 +13,29 @@ const fetchSignInResult = (id, password) => {
     .catch(error => "error");
 };
 
-export { fetchSignInResult };
+const fetchAllUsers = () => {
+  return fetch(serverUrl + "users/all", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+    .then(res => res.json())
+    .then(response => response)
+    .catch(error => "error");
+};
+
+const fetchGradeChange = upOrDown => {
+  return fetch(serverUrl + "users/change-grade", {
+    method: "POST",
+    body: JSON.stringify({ upOrDown }),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+    .then(res => res.json())
+    .then(response => response)
+    .catch(error => "error");
+};
+
+export { fetchSignInResult, fetchAllUsers };
