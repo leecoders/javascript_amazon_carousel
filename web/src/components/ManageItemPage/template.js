9b89b1f7b0e467f9a912623f32311519d4c4bf73
leecoders <als-1994@hanmail.net>
2019-09-25 18:44:26 +0900

    feat: 관리자 페이지 -> 아이템 관리 -> 타이틀 부분 UI

--- web/src/components/ManageItemPage/template.js
+++ web/src/components/ManageItemPage/template.js
@@ -1,6 +1,15 @@
 export default /*html*/ `
 <div class="manage-item-container">
-<div class="manage-item-header">아이템 관리</div>
+    <div class="manage-item-header">
+        <div class="manage-item-title">아이템 관리</div>
+        <div class="manage-item-catecory-container">
+            <div class="item-category"><span>상의</span></div>
+            <div class="item-category"><span>아우터</span></div>
+            <div class="item-category"><span>하의</span></div>
+            <div class="item-category"><span>신발</span></div>
+            <div class="item-category"><span>모자</span></div>
+        </div>
+    </div>
     <div class="item-list-container">
         <div class="item-list">1</div>
         <div class="item-list">2</div>
