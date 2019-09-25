57b9524a448f1024575978fb0b8f60b9b8066cbc
leecoders <als-1994@hanmail.net>
2019-09-25 14:34:51 +0900

    feat: 관리자 페이지 -> 사용자 관리, 아이템 관리 레이아웃

--- web/src/components/ManageItemPage/template.js
+++ web/src/components/ManageItemPage/template.js
@@ -1,5 +1,11 @@
 export default /*html*/ `
 <div class="manage-item-container">
-     아이템 관리 페이지입니다.
+<div class="manage-item-header">아이템 관리</div>
+    <div class="item-list-container">
+        <div class="item-list">1</div>
+        <div class="item-list">2</div>
+        <div class="item-list">3</div>
+        <div class="item-list">4</div>
+    </div>
 </div>
 `;
--- web/src/components/ManageUserPage/template.js
+++ web/src/components/ManageUserPage/template.js
@@ -1,5 +1,11 @@
 export default /*html*/ `
 <div class="manage-user-container">
-유저관리 페이지입니다.
+    <div class="manage-user-header">사용자 관리</div>
+    <div class="user-list-container">
+        <div class="user-list">1</div>
+        <div class="user-list">2</div>
+        <div class="user-list">3</div>
+        <div class="user-list">4</div>
+    </div>
 </div>
 `;
