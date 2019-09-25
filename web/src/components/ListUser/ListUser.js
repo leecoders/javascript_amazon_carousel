8eb51b92fc67a282cabb5fa6baae76c6346db584
leecoders <als-1994@hanmail.net>
2019-09-25 15:53:18 +0900

    update: 관리자 페이지 -> 사용자 관리 UI -> 유저 정보 타이틀 UI

--- web/src/components/ListUser/ListUser.js
+++ web/src/components/ListUser/ListUser.js
@@ -13,13 +13,22 @@ class ListUser {
     <div class="user-container" id="user-${this.userIdx}">
         <div class="user-image"></div>
         <div class="user-info">
-            <div class="user-id" id="user-id-${this.userIdx}">아이디</div>
-            <div class="user-name" id="user-name-${this.userIdx}">이름</div>
-            <div class="user-grade" id="user-grade-${this.userIdx}">등급</div>
+            <div class="user-id">
+                <span class="user-info-title">ID</span>
+                <span id="user-id-${this.userIdx}">123</span>
+            </div>
+            <div class="user-name">
+                <span class="user-info-title">NAME</span>
+                <span id="user-name-${this.userIdx}">123</span>
+            </div>
+            <div class="user-grade">
+                <span class="user-info-title">GRADE</span>
+                <span id="user-grade-${this.userIdx}">123</span>
+            </div>
         </div>
         <div class="user-grade-button-container">
-            <button class="user-grade-up-button" id="user-grade-up-${this.userIdx}">UP</button>
-            <button class="user-grade-down-button" id="user-grade-down-${this.userIdx}">DOWN</button>
+            <button type="button" class="user-grade-up-button" id="user-grade-up-${this.userIdx}">UP</button>
+            <button type="button" class="user-grade-down-button" id="user-grade-down-${this.userIdx}">DOWN</button>
         </div>
     </div>
     `;
